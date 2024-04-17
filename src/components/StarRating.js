673e5f5ccb4a219a9cc53a91e996146ce25b67a5
LyesBesylex <elyaslarfi4am8@gmail.com>
2024-04-16 19:53:20 -0600

    star

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,65 +1,26 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
-import { BsStarFill } from 'react-icons/bs';
-import { Image } from 'react-bootstrap';
-import StarRating from './StarRating'; 
-import ReviewSection from './ReviewSection';
+import React, { useState } from 'react';
 
-class MovieDetail extends Component {
-    render() {
-        const DetailInfo = () => {
-            if (!this.props.selectedMovie) {
-                return <div>Loading....</div>;
-            }
+const StarRating = ({ onChange }) => {
+  const [rating, setRating] = useState(0);
 
-            return (
-                <Card>
-                    <Card.Header>Movie Detail</Card.Header>
-                    <Card.Body>
-                        <Image className="image" src={this.props.selectedMovie.imageUrl} thumbnail />
-                    </Card.Body>
-                    <ListGroup>
-                        <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
-                        <ListGroupItem>
-                            {this.props.selectedMovie.actors.map((actor, i) => (
-                                <p key={i}>
-                                    <b>{actor.actorName}</b> {actor.characterName}
-                                </p>
-                            ))}
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            <h4><BsStarFill /> {this.props.selectedMovie.avgRating}</h4>
-                            {/* Render the StarRating component */}
-                            <StarRating />
-                        </ListGroupItem>
-                    </ListGroup>
-                    <Card.Body>
-                        {this.props.selectedMovie.movieReviews.map((review, i) => (
-                            <p key={i}>
-                                <b>{review.username}</b>&nbsp; {review.review}
-                                &nbsp; <BsStarFill /> {review.rating}
-                            </p>
-                        ))}
-                    </Card.Body>
-                </Card>
-            );
-        };
+  const handleStarClick = (selectedRating) => {
+    setRating(selectedRating);
+    onChange(selectedRating);
+  };
 
-        return (
-            <>
-                <DetailInfo />
-                {/* Render the ReviewSection component */}
-                <ReviewSection />
-            </>
-        );
-    }
-}
-
-const mapStateToProps = state => {
-    return {
-        selectedMovie: state.movie.selectedMovie
-    };
+  return (
+    <div>
+      {[1, 2, 3, 4, 5].map((star) => (
+        <span
+          key={star}
+          onClick={() => handleStarClick(star)}
+          style={{ cursor: 'pointer', color: star <= rating ? 'gold' : 'gray' }}
+        >
+          &#9733;
+        </span>
+      ))}
+    </div>
+  );
 };
 
-export default connect(mapStateToProps)(MovieDetail);
+export default StarRating;
